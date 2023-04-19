0c5c6e0435d853d0806c00478bf85c00079f6976
Clara <clarapachecodev@gmail.com>
2023-04-19 19:50:44 -0300

    Add user parameter to the route and send this information in the request response's message

--- src/server.js
+++ src/server.js
@@ -2,8 +2,11 @@ const express = require("express")
 
 const app =  express()
 
-app.get("/message/:id", (request, response) => {
-  response.send(`Message's ID: ${request.params.id}`)
+app.get("/message/:id/:user", (request, response) => {
+  response.send(`
+  Message's ID: ${request.params.id},
+  User's name: ${request.params.user}
+  `)
 })
 
 const PORT = 3333
