b2a65b015c7cb14618b44bf7e3434802ffb85d44
Clara <clarapachecodev@gmail.com>
2023-04-21 19:05:23 -0300

    Import routes from index.js and ask app to use those routes

--- src/server.js
+++ src/server.js
@@ -1,15 +1,11 @@
 const express = require("express")
 
+const routes = require("./routes/index")
+
 const app =  express()
 app.use(express.json())
+app.use(routes)
 
-
-app.post("/users", (request,response) =>{
-
-  const { name, email, password } =request.body
-  
-  response.json({ name, email, password })
-})
 
 const PORT = 3333
 
